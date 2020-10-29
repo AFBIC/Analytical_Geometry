c7f562b54e533db0d8867bd22aebd30644175a8e
Shane (Treasure) Xue <qq2198631773@outlook.com>
2020-10-29 19:58:11 +0800

    Update jieji.py

--- jieji.py
+++ jieji.py
@@ -1,5 +1,10 @@
 # Analitical (and also normal) geometry library for python
 # The name was just for convenience.
+import matplotlib
+import math
+from matplotlib import pyplot as plt
+
+
 class Point():
   """2d point on decartian plane"""
   
@@ -13,4 +18,19 @@ class Point():
   def get_y(self):
     return self.y_coord
     
+  def __eq__(self , other):
+    return (self.x_coord == other.x_coord && self.y_coord == other.y_coord)
   
+  def __ne__(self , other):
+    return !(self == other)
+  
+  def distance(self , other):
+    """gets distance between 2 points self and other"""
+    
+    return math.sqrt(((self.x_coord - other.x_coord)**2) + ((self.y_coord - other.y_coord)**2))
+  
+  def move(self , x , y):
+    """moves point x units up and y units down"""
+    
+    self.x_coord = self.x_coord + x
+    self.y_coord = self.y_coord + y
