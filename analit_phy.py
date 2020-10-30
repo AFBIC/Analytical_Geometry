36f3d20b2dae6214c984e672907de391f8289887
Shane (Treasure) Xue <qq2198631773@outlook.com>
2020-10-30 13:59:37 +0800

    Create analit_phy.py

---
+++ analit_phy.py
@@ -0,0 +1,17 @@
+# analitical physics
+
+class Mass():
+  def __init__(self,mass,ratio_high=1,ratio_low=1)
+    self.mass=mass
+    self.ratio_high=ratio_high
+    self.ratio_low=ratio_low
+
+class Mass_Point():
+  
+  def __init__(self,x=0,y=0,z=0,mass=Mass(0),speed=Speed(0),accel=Accel(0)):
+    self.x=x
+    self.y=y
+    self.z=z
+    self.mass=mass
+    self.speed=speed
+    self.accel
