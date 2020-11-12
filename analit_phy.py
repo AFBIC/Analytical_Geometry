b2d6a46e059f101014d400ce281e6f34d6078a68
Shane (Treasure) Xue <qq2198631773@outlook.com>
2020-11-12 20:20:14 +0800

    Update analit_phy.py

--- analit_phy.py
+++ analit_phy.py
@@ -2,29 +2,29 @@
 import jieji
 
 class Force():
-  """
-  A force.
-  Note that the force is stray. It does not do anything to any object.
-  It only has a direction and magnitude, and can be binded to an obj.
-  """
-  def __init__(self,vector):
     """
-    _vector is a vactor, which has a direction and magnitude
+    A force.
+    Note that the force is stray. It does not do anything to any object.
+    It only has a direction and magnitude, and can be binded to an obj.
     """
-    self._vector = vector
+    def __init__(self,vector):
+        """
+        _vector is a vector, which has a direction and magnitude
+        """
+        self._vector = vector
     
-  def increase(self,increase_by)
-    self.vector.magnitude += increase_by
+    def increase(self,increase_by):
+        self.vector.magnitude += increase_by
   
 
 
 class Mass_Point():
   
-  def __init__(self,mass,x=0,y=0,z=0):
-    self.x=x
-    self.y=y
-    self.z=z
-    self.mass=mass
+    def __init__(self,mass,x=0,y=0,z=0):
+        self.x=x
+        self.y=y
+        self.z=z
+        self.mass=mass
   
-  def Apply_Force(self,Force):
-    pass
+    def Apply_Force(self,Force):
+        pass
